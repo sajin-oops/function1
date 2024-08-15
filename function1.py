335e5c1f92640eabd4b3faccdb05b04cefcf9c9e
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-15 20:36:03 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,10 @@
+def myfun(name): # name = parameter
+  print("Hello there")
+  print("Hey wass up " + name)
+  
+myfun("sajin") # sajin = passing arguments
+
+'''
+Hello there
+Hey wass up sajin
+'''
